8dd90385728c96bddff129172556a18b61445235
Priyanka <arora.priyanka2014@gmail.com>
2019-08-30 17:45:36 +0530

    this is commit from user x

--- src/test/java/qaclickacademy/APITest.java
+++ src/test/java/qaclickacademy/APITest.java
@@ -9,11 +9,17 @@ public class APITest {
 	public void postJira()
 	{
 		System.out.println("postJira");
+		System.out.println("postJira1");
+		System.out.println("postJira2");
+		System.out.println("postJira3");
 	}
 	
 	@Test
 	public void deleteTwitter()
 	{
+		System.out.println("deleteTwitter");
+		System.out.println("deleteTwitter");
+		System.out.println("deleteTwitter");
 		System.out.println("deleteTwitter");
 	}
 
